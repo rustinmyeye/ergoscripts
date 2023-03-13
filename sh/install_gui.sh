53c37714ef47051fa6e7ca212fd96b5559dcf4da
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2023-03-13 15:04:56 -0400

    Update install_gui.sh

--- sh/install_gui.sh
+++ sh/install_gui.sh
@@ -21,64 +21,64 @@ set_environment(){
     let PERCENT_HEADERS=100
 
     # Check for python
-    if ! hash python3; then
-        echo "python is not installed"
+    #if ! hash python3; then
+        #echo "python is not installed"
         #curl https://pyenv.run | bash
         #echo "Python installed, please re-run"
         #https://github.com/pyenv-win/pyenv-win
-        exit 1
-    fi
+        #exit 1
+    #fi
    
     # check python version
-    pyv=$(python -V 2>&1 | sed 's/.* \([0-9]\).\([0-9]\).*/\1\2/')
+    #pyv=$(python -V 2>&1 | sed 's/.* \([0-9]\).\([0-9]\).*/\1\2/')
     #echo $pyv
 
     # Check Java
-    jver=`java -version 2>&1 | grep 'version' 2>&1 | awk -F\" '{ split($2,a,"."); print a[1]"."a[2]}'`
-    if [[ $jver > "1.8" ]]; then                
-        echo "."
-        echo "."
+    #jver=`java -version 2>&1 | grep 'version' 2>&1 | awk -F\" '{ split($2,a,"."); print a[1]"."a[2]}'`
+    #if [[ $jver > "1.8" ]]; then                
+        #echo "."
+        #echo "."
         #exit 1
-    else
-        echo "Java="$jver
-    fi
+    #else
+        #echo "Java="$jver
+    #fi
    
   
     # Set heap
-    case "$(uname -s)" in
+    #case "$(uname -s)" in
 
-        CYGWIN*|MINGW32*|MSYS*|MINGW*)
-            echo 'MS Windows'
-            WIN_MEM=$(systeminfo)
-            WIN_MEM=$(wmic OS get FreePhysicalMemory)
-            kb_to_mb=$((memory*1024))
-            echo "WIN memory !!-- " $kb_to_mb
-            JVM_HEAP_SIZE="-Xmx${kb_to_mb}m"
-            ;;
+        #CYGWIN*|MINGW32*|MSYS*|MINGW*)
+            #echo 'MS Windows'
+            #WIN_MEM=$(systeminfo)
+            #WIN_MEM=$(wmic OS get FreePhysicalMemory)
+            #kb_to_mb=$((memory*1024))
+            #echo "WIN memory !!-- " $kb_to_mb
+            #JVM_HEAP_SIZE="-Xmx${kb_to_mb}m"
+            #;;
 
-        Linux)
-            memory=`awk '/MemTotal/ {printf( "%d\n", $2 / 1024 )}' /proc/meminfo` 
-            half_mem=$((${memory%.*} / 2))
-            JVM_HEAP_SIZE="-Xmx${half_mem}m"
-            ;;
+        #https://raw.githubusercontent.com/rustinmyeye/ergoscripts/main/sh/install_gui.shLinux)
+            #memory=`awk '/MemTotal/ {printf( "%d\n", $2 / 1024 )}' /proc/meminfo` 
+            #half_mem=$((${memory%.*} / 2))
+            #JVM_HEAP_SIZE="-Xmx${half_mem}m"
+            #;;
 
-        Darwin) #Other
-            memory=$(top -l1 | awk '/PhysMem/ {print $2}')
-            half_mem=$((${memory%?} / 2))
-            JVM_HEAP_SIZE="-Xmx${half_mem}g"             
-            ;;
+        #Darwin) #Other
+            #memory=$(top -l1 | awk '/PhysMem/ {print $2}')
+            #half_mem=$((${memory%?} / 2))
+            #JVM_HEAP_SIZE="-Xmx${half_mem}g"             
+            #;;
 
-        Other*)
-            memory=`awk '/MemTotal/ {printf( "%d\n", $2 / 1024 )}' /proc/meminfo` 
-            half_mem=$((${memory%.*} / 3))
-            JVM_HEAP_SIZE="-Xmx${half_mem}m"
-            ;;
-    esac
+        #Other*)
+            #memory=`awk '/MemTotal/ {printf( "%d\n", $2 / 1024 )}' /proc/meminfo` 
+            #half_mem=$((${memory%.*} / 3))
+            #JVM_HEAP_SIZE="-Xmx${half_mem}m"
+            #;;
+    #esac
 
-    case "$(uname -m)" in
-        armv7l|aarch64)
-            JVM_HEAP_SIZE="-Xmx2G"
-            echo "JVM_HEAP_SIZE Set to:" $JVM_HEAP_SIZE
+    #case "$(uname -m)" in
+        #armv7l|aarch64)
+            #JVM_HEAP_SIZE="-Xmx2G"
+            #echo "JVM_HEAP_SIZE Set to:" $JVM_HEAP_SIZE
             
             #echo "Raspberry Pi detected, running node in light-mode" 
 
@@ -90,119 +90,119 @@ set_environment(){
 
             #sleep 10
 
-            ;;
-    esac
+            #;;
+    #esac
     
-}
+#}
 
-set_configuration (){
-        echo "
-                ergo {
-                    node {
+#set_configuration (){
+        #echo "
+                #ergo {
+                    #node {
                         # Full options available at 
                         # https://github.com/ergoplatform/ergo/blob/master/src/main/resources/application.conf
                         
-                        mining = false
+                        #mining = false
                         # Skip validation of transactions in the mainnet before block 417,792 (in v1 blocks).
                         # Block 417,792 is checkpointed by the protocol (so its UTXO set as well).
                         # The node still applying transactions to UTXO set and so checks UTXO set digests for each block.
-                        skipV1TransactionsValidation = true
-                    }
-                }      
+                        #skipV1TransactionsValidation = true
+                    #}
+                #}      
                         
-                scorex {
-                    restApi {
+                #scorex {
+                    #restApi {
                         # Hex-encoded Blake2b256 hash of an API key. 
                         # Should be 64-chars long Base16 string.
                         # below is the hash of the string 'hello'
                         # replace with your actual hash 
-                        apiKeyHash = "$BLAKE_HASH"
+                        #apiKeyHash = "$BLAKE_HASH"
                         
-                    }
+                    #}
                 
-                }
-        " > ergo.conf
+                #}
+        #" > ergo.conf
 
-}
+#}
 
-start_node(){
-    java -jar $JVM_HEAP_SIZE ergo.jar --mainnet -c ergo.conf > server.log 2>&1 & 
-    echo "#### Waiting for a response from the server. ####"
-    while ! curl --output /dev/null --silent --head --fail http://localhost:9053; do sleep 1 && error_log; done;  # wait for node be ready with progress bar
+#start_node(){
+    #java -jar $JVM_HEAP_SIZE ergo.jar --mainnet -c ergo.conf > server.log 2>&1 & 
+    #echo "#### Waiting for a response from the server. ####"
+    #while ! curl --output /dev/null --silent --head --fail http://localhost:9053; do sleep 1 && error_log; done;  # wait for node be ready with progress bar
     
-}
+#}
 
 # Set basic config for boot, boot & get the hash and then re-set config 
-first_run() {
+#first_run() {
 
             
         ### Download the latest .jar file                                                                    
-        if [ ! -e *.jar ]; then 
-            echo "- Retrieving latest node release.."
-            LATEST_ERGO_RELEASE=$(curl -s "https://api.github.com/repos/ergoplatform/ergo/releases/latest" | awk -F '"' '/tag_name/{print $4}')
-            LATEST_ERGO_RELEASE_NUMBERS=$(echo ${LATEST_ERGO_RELEASE} | cut -c 2-)
-            ERGO_DOWNLOAD_URL=https://github.com/ergoplatform/ergo/releases/download/${LATEST_ERGO_RELEASE}/ergo-${LATEST_ERGO_RELEASE_NUMBERS}.jar
-            echo "- Downloading Latest known Ergo release: ${LATEST_ERGO_RELEASE}."
-            curl --silent -L ${ERGO_DOWNLOAD_URL} --output ergo.jar
-        fi 
+        #if [ ! -e *.jar ]; then 
+            #echo "- Retrieving latest node release.."
+            #LATEST_ERGO_RELEASE=$(curl -s "https://api.github.com/repos/ergoplatform/ergo/releases/latest" | awk -F '"' '/tag_name/{print $4}')
+            #LATEST_ERGO_RELEASE_NUMBERS=$(echo ${LATEST_ERGO_RELEASE} | cut -c 2-)
+            #ERGO_DOWNLOAD_URL=https://github.com/ergoplatform/ergo/releases/download/${LATEST_ERGO_RELEASE}/ergo-${LATEST_ERGO_RELEASE_NUMBERS}.jar
+            #echo "- Downloading Latest known Ergo release: ${LATEST_ERGO_RELEASE}."
+            #curl --silent -L ${ERGO_DOWNLOAD_URL} --output ergo.jar
+        #fi 
 
     
         
         # API 
-        read -p "
+        #read -p "
     #### Please create a password. #### 
-    This will be used to unlock your API. 
-    Generally using the same API key through the entire sync process can prevent 'Bad API Key' errors:
-    " input
+    #This will be used to unlock your API. 
+    #Generally using the same API key through the entire sync process can prevent 'Bad API Key' errors:
+    #" input
 
-        export API_KEY=$input
-        echo "$API_KEY" > api.conf
+        #export API_KEY=$input
+        #echo "$API_KEY" > api.conf
 
         
         
-        start_node
+        #start_node
         
-        export BLAKE_HASH=$(curl --silent -X POST "http://localhost:9053/utils/hash/blake2b" -H "accept: application/json" -H "Content-Type: application/json" -d "\"$input\"")
-        echo "$BLAKE_HASH" > blake.conf
+        #export BLAKE_HASH=$(curl --silent -X POST "http://localhost:9053/utils/hash/blake2b" -H "accept: application/json" -H "Content-Type: application/json" -d "\"$input\"")
+        #echo "$BLAKE_HASH" > blake.conf
         #echo "BLAKE_HASH:$BLAKE_HASH"
         
-        func_kill
+        #func_kill
 
         # Add blake hash
-        set_configuration
+        #set_configuration
         
-        start_node
+        #start_node
         
         # Add blake hash
-        set_configuration
+        #set_configuration
 
-}
+#}
 
-func_kill(){
-    case "$(uname -s)" in
+#func_kill(){
+    #case "$(uname -s)" in
 
-    CYGWIN*|MINGW32*|MSYS*|MINGW*)
-        echo 'MS Windows'
-        netstat -ano | findstr :9053
-        taskkill /PID 9053 /F
-        ;;
+    #CYGWIN*|MINGW32*|MSYS*|MINGW*)
+        #echo 'MS Windows'
+        #netstat -ano | findstr :9053
+        #taskkill /PID 9053 /F
+        #;;
 
-    armv7l*|aarch64)
-        echo "on Pi!"
+    #armv7l*|aarch64)
+        #echo "on Pi!"
         #kill -9 $(lsof -t -i:9053)
         #kill -9 $(lsof -t -i:9030)
-        killall -9 java
-        sleep 10
-        ;;
-    *) #Other
-        kill -9 $(lsof -t -i:9053)
-        kill -9 $(lsof -t -i:9030)
-        killall -9 java
-        sleep 10
-        ;;
-    esac
+        #killall -9 java
+        #sleep 10
+        #;;
+    #*) #Other
+        #kill -9 $(lsof -t -i:9053)
+        #kill -9 $(lsof -t -i:9030)
+        #killall -9 java
+        #sleep 10
+        #;;
+    #esac
 
-}
+#}
 
 error_log(){
     inputFile=ergo.log
@@ -224,7 +224,7 @@ error_log(){
         echo i: $i
         #func_kill
         curl -X POST --max-time 10 "http://127.0.0.1:9053/node/shutdown" -H "api_key: $API_KEY"
-        start_node
+        #start_node
         
     fi
 
@@ -239,9 +239,9 @@ check_status(){
     
     if [ -z "$string" ]; then
         echo -e "${LRED}${1} is down${NC}"
-        func_kill
+        #func_kill
         
-        start_node
+        #start_node
         print_console
     else
        echo -e "${LGREEN}${1} is online${NC}"
